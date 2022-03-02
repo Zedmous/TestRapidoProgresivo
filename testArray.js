ccef0b20332650376a06009728556ff96190e597
zedmous <ejnsilva29@gmail.com>
2022-03-02 04:08:10 -0400

    se me olvido xd

--- testArray.js
+++ testArray.js
@@ -1,34 +1,40 @@
-var numeros = [1, 2, 5, 4];
+var numeros = [1, 2, 3, 2, 2, 4, 5, 6, 7];
 var isProgressive = function (numeros) {
     var i = 0;
     var enc = true;
+    var cont = 0;
+    var pos = [];
     while (i < numeros.length) {
-        if (numeros[i] > numeros[i + 1]) {
+        if (numeros[i] > numeros[i + 1] && i == 0) {
+        }
+        else if (numeros[i] > numeros[i + 1] && numeros[i] > numeros[i - 1] && (i > 0 && i < numeros.length - 1)) {
             enc = false;
+            cont++;
+            pos.push(i);
+        }
+        else if (i == numeros.length - 1) {
             i = numeros.length;
         }
         i++;
     }
-    return enc;
+    return { enc: enc, pos: pos, cont: cont };
 };
 console.log("Visualizando arreglo");
 console.log(numeros);
-var enc = isProgressive(numeros);
-if (enc) {
+var datos = isProgressive(numeros);
+if (datos.enc) {
     console.log("Es un arreglo progresivo");
 }
 else {
-    console.log("No es un arreglo progresivo");
-    console.log("Se procede a eliminar un elemento");
-    var posicion = Math.round(Math.random() * numeros.length);
-    console.log("Posicion elegida: ", posicion - 1);
-    numeros.splice(posicion - 1, 1);
-    if (isProgressive(numeros)) {
+    numeros.splice(datos.pos[0] + 1, 1);
+    var datos2 = isProgressive(numeros);
+    console.log("Visualizando arreglo");
+    console.log(numeros);
+    if (datos2.enc) {
         console.log("Es un arreglo progresivo");
     }
     else {
         console.log("No es un arreglo progresivo");
     }
-    console.log(numeros);
-    /**/
+    //console.log("No es un arreglo progresivo");
 }
--- testArray.ts
+++ testArray.ts
@@ -1,39 +1,41 @@
-let numeros: number[] = [1, 2, 5,4];
+let numeros: number[] = [1, 2, 3, 2,2, 4, 5, 6, 7];
 let isProgressive = (numeros: number[]) => {
     let i = 0;
     let enc = true;
+    let cont = 0;
+    let pos: number[] = [];
     while (i < numeros.length) {
-        if (numeros[i] > numeros[i + 1]) {
+        if(numeros[i] > numeros[i + 1] && i==0){
+
+        }else if (numeros[i] > numeros[i + 1] && numeros[i] > numeros[i - 1]  && (i>0 && i<numeros.length-1)) {
             enc = false;
-            i = numeros.length;
+            cont++;
+            pos.push(i);
+        }else if(i==numeros.length-1){
+            i=numeros.length;
         }
         i++;
     }
-    return enc;
+    return { enc, pos, cont };
 }
-
 
 console.log("Visualizando arreglo")
 console.log(numeros)
-
-let enc = isProgressive(numeros);
-
-
-if (enc) {
+let datos = isProgressive(numeros);
+if (datos.enc) {
     console.log("Es un arreglo progresivo")
 } else {
-    console.log("No es un arreglo progresivo");
-    console.log("Se procede a eliminar un elemento");
-    let posicion: number = Math.round(Math.random() * numeros.length);
-    console.log("Posicion elegida: ", posicion-1);
-    numeros.splice(posicion-1, 1);
-    if (isProgressive(numeros)) {
+
+    numeros.splice(datos.pos[0] + 1, 1)
+    let datos2 = isProgressive(numeros);
+    console.log("Visualizando arreglo")
+    console.log(numeros)
+    if (datos2.enc) {
         console.log("Es un arreglo progresivo")
     } else {
         console.log("No es un arreglo progresivo");
     }
-    console.log(numeros)
-    /**/
 
+    //console.log("No es un arreglo progresivo");
 }
 
